3742d90f9d2808c93b0085008494abf185c106a6
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-11 13:07:33 -0300

    Solução do exercício Olá usuário

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercicio_OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomedousuário;
+            Console.Write("Digite seu nome: ");
+            nomedousuário= Console.ReadLine();
+
+            Console.WriteLine($"Olá {nomedousuário}!");
+        }
+    }
+}
